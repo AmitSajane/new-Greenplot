d467654a0847ac40b58d8a4a1a85424d2c3b77ad
Niranjangouda Patil <154399232+NiranjangoudaPatil@users.noreply.github.com>
2026-07-08 19:49:10 +0530

    Added newline

--- src/screens/farmerHome/FarmerHomeScreen.tsx
+++ src/screens/farmerHome/FarmerHomeScreen.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { FarmerHomeContent } from './components/FarmerHomeContent';
 import { useFarmerHome } from './hooks/useFarmerHome';
 
+
+
 export default function FarmerHomeScreen() {
   const vm = useFarmerHome();
   return <FarmerHomeContent {...vm} />;
